a04d5978f7fccccab12ad886f9f9dc7b8e8630c4
Yurii Mashtalir <mashtaliryurko@gmail.com>
2017-01-30 11:41:58 +0200

    Initial commit

---
+++ maps/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+import pandas as pd
+import os
+
+
+# Create your views here.
+def index(request):
+    return leaflet(request)
+
+
+def googlemapstrees(request):
+    return render(request, 'googlemaptrees.html')
+
+
+def cars(request):
+    return render(request, 'cars.html')
+
+
+def carsandtrees(request):
+    return render(request, 'carsandtrees.html')
+
+def houses(request):
+    return render(request, 'houses.html')
+
+def united(request):
+    return render(request, 'united.html')
+
+def leaflet(request):
+    return render(request, 'leaflet.html')
+
+
+def trees_json(request):
+    module_dir = os.path.dirname(__file__)  # get current directory
+    file_path = os.path.join(module_dir, 'static/trees_GeoCoo.json')
+    with open(file_path) as json_file:
+        json_trees = json_file.read()
+    return HttpResponse(json_trees, content_type="application/json")
+
+
+def cars_json(request):
+    module_dir = os.path.dirname(__file__)  # get current directory
+    file_path = os.path.join(module_dir, 'static/cars_GeoCoo.json')
+    with open(file_path) as json_file:
+        json_cars = json_file.read()
+    return HttpResponse(json_cars, content_type="application/json")
+
+
+def houses_json(request):
+    module_dir = os.path.dirname(__file__)  # get current directory
+    file_path = os.path.join(module_dir, 'static/housesGeo.json')
+    with open(file_path) as json_file:
+        json_houses = json_file.read()
+    return HttpResponse(json_houses, content_type="application/json")
+
+def roads_json(request):
+    module_dir = os.path.dirname(__file__)  # get current directory
+    file_path = os.path.join(module_dir, 'static/roadsGeo.json')
+    with open(file_path) as json_file:
+        json_roads = json_file.read()
+    return HttpResponse(json_roads, content_type="application/json")
---
+++ yarval/urls.py
@@ -0,0 +1,35 @@
+"""yarval URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+
+admin.autodiscover()
+import maps.views
+
+urlpatterns = [
+    url(r'^$', maps.views.index, name='index'),
+    url(r'^trees$', maps.views.googlemapstrees, name='trees'),
+    url(r'^cars$', maps.views.cars, name='cars'),
+    url(r'^carsandtrees$', maps.views.carsandtrees, name='carsandtrees'),
+    url(r'^houses$', maps.views.houses, name='houses'),
+    url(r'^united$', maps.views.united, name='united'),
+    url(r'^leaflet$', maps.views.leaflet, name='leaflet'),
+    url(r'^trees_json$', maps.views.trees_json, name='trees_json'),
+    url(r'^cars_json$', maps.views.cars_json, name='cars_json'),
+    url(r'^houses_json$', maps.views.houses_json, name='houses_json'),
+    url(r'^roads_json$', maps.views.roads_json, name='roads_json'),
+    url(r'^admin/', include(admin.site.urls)),
+]
