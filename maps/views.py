e867e5c681dc060336124188310720a7b2c40554
proggeo <mashtaliryurii@knu.ua>
2017-02-13 22:02:56 +0200

    project cleaning

--- maps/views.py
+++ maps/views.py
@@ -31,25 +31,3 @@ def svgswitcher(request):
 def pedestrian(request):
     return render(request, 'pedestrian.html')
 
-
-def get_metrotimer(request):
-    # locale.setlocale(locale.LC_ALL,'uk_ua.cp1251')
-    stations = metrotimer.get_all_stations_ukr()
-    context = {
-        'stationsUK': sorted(stations.values()),
-        'stationsEN': sorted(stations.keys())
-    }
-    return render(request, 'metrotimer.html', context)
-
-@csrf_exempt
-def post_metrotimer(request):
-    origin_station = request.POST['station']
-    times_en = metrotimer.get_all_times(origin_station)
-    times_ua = dict()
-    stations_ua = metrotimer.get_all_stations_ukr()
-    for key in times_en:
-        times_en[key] = str(int(times_en[key] / 60)) + '.' + str(times_en[key] % 60)
-        times_ua[stations_ua[key]] = times_en[key]
-    response = JsonResponse({'times_en': sorted(times_en.items()), 'times_ua': sorted(times_ua.items())})
-
-    return response
